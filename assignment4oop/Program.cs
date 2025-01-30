e5be5466a18510e1f97a8ccefb0b0cb6ace4ad49
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-30 18:06:28 +0200

    q1

--- assignment4oop/Program.cs
+++ assignment4oop/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-           
+            #region q1
+            Calculator calculator = new Calculator();
+
+            int sum1 = calculator.Add(5, 10);
+            int sum2 = calculator.Add(1, 2, 3);
+            double sum3 = calculator.Add(4.5, 5.5);
+
+            Console.WriteLine($"Sum of 5 and 10: {sum1}");      
+            Console.WriteLine($"Sum of 1, 2, and 3: {sum2}");  
+            Console.WriteLine($"Sum of 4.5 and 5.5: {sum3}");
+            #endregion
         }
     }
 }
