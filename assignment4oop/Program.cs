9e4c761beaebeb1d2fceb081033c3a7827a09fad
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-01-30 18:22:26 +0200

    q2

--- assignment4oop/Program.cs
+++ assignment4oop/Program.cs
@@ -5,15 +5,25 @@
         static void Main(string[] args)
         {
             #region q1
-            Calculator calculator = new Calculator();
+            //Calculator calculator = new Calculator();
+            //int sum1 = calculator.Add(5, 10);
+            //int sum2 = calculator.Add(1, 2, 3);
+            //double sum3 = calculator.Add(4.5, 5.5);
 
-            int sum1 = calculator.Add(5, 10);
-            int sum2 = calculator.Add(1, 2, 3);
-
-            Console.WriteLine($"Sum of 5 and 10: {sum1}");      
-            Console.WriteLine($"Sum of 1, 2, and 3: {sum2}");  
-            Console.WriteLine($"Sum of 4.5 and 5.5: {sum3}");
+            //Console.WriteLine($"Sum of 5 and 10: {sum1}");     
+            //Console.WriteLine($"Sum of 1, 2, and 3: {sum2}");  
+            //Console.WriteLine($"Sum of 4.5 and 5.5: {sum3}");
             #endregion
+            #region q2
+            Rectangle rect1 = new Rectangle();
+            Rectangle rect2 = new Rectangle(10, 20);
+            Rectangle rect3 = new Rectangle(15);
+   
+            rect1.DisplayDimensions();
+            rect2.DisplayDimensions(); 
+            rect3.DisplayDimensions(); 
         }
+        #endregion
     }
+    
 }
